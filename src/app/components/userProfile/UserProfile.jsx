299635897c3880951dba3c20d4ed3b4064a1d5ea
IgorBrizola <igorbrizzola@gmail.com>
2025-11-13 17:11:22 -0300

    feat: add api of random image profile

--- src/app/components/userProfile/UserProfile.jsx
+++ src/app/components/userProfile/UserProfile.jsx
@@ -1,18 +1,69 @@
-import React from "react";
-
-import Styles from './userProfile.module.css'
+"use client";
+import { useEffect, useState } from "react";
+import Styles from "./userProfile.module.css";
 
 const UserProfile = () => {
+  const [avatar, setAvatar] = useState("");
+  const [email, setEmail] = useState("");
+  const [first, setFirst] = useState("");
+  const [last, setLast] = useState("");
+
+  const [course, setCourse] = useState("");
+
+  const courses = [
+    { sigla: "S.I 💻", nome: "Sistemas da informação" },
+    { sigla: "C.C 💻", nome: "Ciência da computação" },
+  ];
+
+  function pickRandomCourse() {
+    const random = courses[Math.floor(Math.random() * courses.length)];
+    return random.sigla; 
+  }
+
+
+  async function fetchAvatar() {
+    try {
+      const res = await fetch("https://randomuser.me/api/");
+      const data = await res.json();
+
+      const url = data.results[0].picture.large;
+      const email = data.results[0].email;
+      const first = data.results[0].name.first;
+      const last = data.results[0].name.last;
+
+      setAvatar(url);
+      setEmail(email);
+      setFirst(first);
+      setLast(last);
+
+      setCourse(pickRandomCourse());
+
+    } catch (error) {
+      console.error("Erro ao carregar avatar:", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchAvatar();
+
+    const interval = setInterval(() => {
+      fetchAvatar();
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <section className={Styles.userProfile}>
       <img
         className={Styles.avatar}
-        src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?auto=format&fit=facearea&facepad=2&w=300&h=300&q=80"
+        src={avatar || null}
         alt="Avatar"
       />
       <div className={Styles.userProfileText}>
-        <h3>Mark Wanner</h3>
-        <p>Cursando S.I</p>
+        <h3>{first} {last}</h3>
+        <p>{email}</p>
+        <p>Cursando {course}</p>
       </div>
     </section>
   );
